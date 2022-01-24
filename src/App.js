6b1f2884c66cec6f0c2511e07584983e98fc1930
Dylan <dylanbarber1@outlook.com>
2022-01-24 04:01:12 +0000

    Clean up and seperate functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import * as axios from 'axios'
+import { useState } from 'react';
 const { ipcRenderer } = window.require("electron");
 
 // const electron = window.require('electron')
@@ -12,53 +13,70 @@ const path = window.require('path')
 
 function App() {
 
-  // function test() {
-  //   const test1 = fs.readFileSync(document.getElementById('file').value)
-  //   console.log(document.getElementById('file').value)
-  // }
+  const [fileInfoState, setFileInfoState] = useState({
+    filePath: undefined,
+    fileName: undefined
+  })
 
-  const submitForm = async () => {
+  const selectFile = async () => {
+
+    // Call to main process for opening file dialog window
     const res = await ipcRenderer.sendSync('openDialog')
-    console.log(res.filePaths[0])
-    // const fileData = await fs.readFileSync(res.filePaths[0], {encoding:'utf8', flag:'r'})
-    // const fileData = await fs.createReadStream(res.filePaths[0], {encoding:'utf8', flag:'r'})
-    const fileData = await fs.readFileSync(res.filePaths[0])
-    const fileBlob = new Blob(fileData)
 
+    const filePath = res.filePaths[0]
 
+    //Get file name, Windows will use backslashes because it's an "awesome" OS
+    let fileName = ''
+    if (process.platform === 'win32') {
+      fileName = filePath.split('\\').pop()
+    } else {
+      fileName = filePath.split('/').pop()
+    }
 
-    const formData = new FormData()
-    formData.append('file', fileBlob, 'readStreamTest.gcode')
-    formData.append('select', true)
-    formData.append('print', false) // TODO: Put conditional check box for starting print immediately here!
-    
+    setFileInfoState({
+      filePath,
+      fileName
+    })
+  }
 
+  const uploadFile = async () => {
 
-    axios
-      .post('http://192.168.0.190/api/files/local', formData, {
-        headers: {
-          ...formData.get('headers'),
-          'X-Api-Key': 'C9FA6D1FD11F452383F4CF773436B70C',
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-    .then((res) => {
-        alert("File Upload success");
-      })
-      .catch((err) => alert("File Upload Error"));
+    const { filePath, fileName } = fileInfoState
+
+    if (!fileInfoState.filePath) {
+      // TODO: Put validation errors
+    } else {
+      // Start the read buffer and slap that bad boy in a Blob for file upload
+      const fileData = await fs.readFileSync(filePath)
+      const fileBlob = new Blob(fileData)
+
+      // Create FormData request object
+      const formData = new FormData()
+      formData.append('file', fileBlob, fileName)
+      formData.append('select', true)
+      formData.append('print', false) // TODO: Put conditional check box for starting print immediately here!
+
+      //Send the axios request
+      axios
+        .post('http://192.168.0.190/api/files/local', formData, {
+          headers: {
+            ...formData.get('headers'),
+            'X-Api-Key': 'C9FA6D1FD11F452383F4CF773436B70C',
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        .then((res) => {
+          alert("File Upload success");
+        })
+        .catch((err) => alert("File Upload Error"));
+    }
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <button onClick={() => test()}>Test</button> */}
-        <input id='file' type='file'></input>
-        <button onClick={async () => await submitForm()}>Test</button>
-      </header>
+      <button onClick={async () => await selectFile()}>Select File</button>
+      {fileInfoState.fileName ?? <p>{fileInfoState.fileName}</p>}
+      <button onClick={async () => await uploadFile()}>Upload File</button>
     </div>
   );
 }

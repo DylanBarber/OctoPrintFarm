2ecf74e2fade095b779df15ff37f44493773234b
Dylan <dylanbarber1@outlook.com>
2022-02-06 16:20:21 +0000

    Completed add printer flow with printer card reload. Still loads of garbage code to clean up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import * as axios from 'axios'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 const { ipcRenderer } = window.require("electron");
 const fs = window.require('fs')
 
@@ -11,6 +11,22 @@ function App() {
     filePath: undefined,
     fileName: undefined
   })
+  const [printerIp, setPrinterIp] = useState('');
+  const [printerApiKey, setPrinterApiKey] = useState('');
+  const [printerDefs, setPrinterDefs] = useState(undefined);
+
+  useEffect(() => {
+    loadPrinterDefs();
+  }, [])
+
+  const loadPrinterDefs = async () => {
+    try {
+      const printerDefsData = await fs.readFileSync('~/Documents/octoFarm/printerDefs.json', { encoding: 'utf-8' });
+      setPrinterDefs(JSON.parse(printerDefsData));
+    } catch (err) {
+      console.warn('No printer definitions file was found. System will assume that no printers have been added');
+    }
+  }
 
   const selectFile = async () => {
 
@@ -67,26 +83,46 @@ function App() {
   };
 
 
-  const [printerIp, setPrinterIp] = useState('');
-  const [printerApiKey, setPrinterApiKey] = useState('');
+
 
   const addPrinter = async () => {
 
     try {
       const fileData = await fs.readFileSync('~/Documents/octoFarm/printerDefs.json', { encoding: 'utf-8' })
-      console.log(fileData)
+      const printerDefData = JSON.parse(fileData);
+      // TODO: Add logic for adding duplicate printer IPs
+      printerDefData.push({
+        printerIp,
+        printerApiKey
+      });
+      await fs.writeFileSync('~/Documents/octoFarm/printerDefs.json', JSON.stringify(printerDefData));
+      loadPrinterDefs();
     } catch (err) {
       if (err.message.includes('no such file or directory')) {
         await fs.mkdirSync('~/Documents/octoFarm', { recursive: true })
         await fs.writeFileSync('~/Documents/octoFarm/printerDefs.json', JSON.stringify([{
-          printerIp: '192.168.0.0',
-          printerApiKey: 'testApiKey'
-        }]), { ecoding: 'utf-8'})
+          printerIp,
+          printerApiKey
+        }]), { ecoding: 'utf-8' })
+        loadPrinterDefs();
 
       } else {
         console.log("Something went wrong... Maybe this application doesn't have read/write access to your filesystem? Try running as administrator")
       }
     }
+  }
+
+  let printerCards = null;
+
+  if (printerDefs) {
+    printerCards = printerDefs.map(printer => {
+      return (
+        <div>
+          <h1>{printer.printerIp}</h1>
+          <h1>{printer.printerApiKey}</h1>
+        </div>
+      )
+    })
   }
 
   return (
@@ -102,6 +138,7 @@ function App() {
       <input value={printerApiKey} onChange={(e) => setPrinterApiKey(e.target.value)}></input>
 
       <button onClick={addPrinter}>Add Printer</button>
+      {printerCards}
     </div>
   );
 }
